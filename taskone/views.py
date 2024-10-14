0765dfe9fb4f91c6a3a6891708054ffa1e7a172e
Dipak <125576159+DipakShinde1196@users.noreply.github.com>
2024-10-14 22:07:44 +0530

    Update views.py

--- taskone/views.py
+++ taskone/views.py
@@ -1,4 +1,3 @@
-# views.py
 import time
 from django.http import HttpResponse
 from .signals import my_signal
@@ -7,10 +6,8 @@ def trigger_signal(request):
     print("Signal sending started")
     start_time = time.time()
 
-    # Send the signal
     my_signal.send(sender=None)
 
-    # Execution time after signal handling
     end_time = time.time()
     duration = end_time - start_time
     print(f"Signal sending finished. Duration: {duration:.2f} seconds")
